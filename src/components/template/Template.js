d46f7668cc9f832d9dcb5cfa3d544a343e02b900
imron.malee <imron.malee@gmail.com>
2024-02-10 01:26:40 +0700

    [index] loading data

--- src/components/template/Template.js
+++ src/components/template/Template.js
@@ -200,7 +200,7 @@ export default function Template() {
                 
                 
             ) : (
-              <>ssss</>
+              <>Loading Data...</>
             )}
             
           </div>
