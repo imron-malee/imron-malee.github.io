20ca9a2ec2ee701b3b0e1b5ef0576044fdfc7b45
imron.malee <imron.malee@gmail.com>
2024-02-10 01:55:33 +0700

    change basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Template from "./components/template/Template";
 function App() {
   return (
     <>
-    <Router basename="/">
+    <Router basename="/a">
       <Routes>
         <Route path="/" element={<Template/>} />
         <Route path="/a/b" element={<Template/>} />
